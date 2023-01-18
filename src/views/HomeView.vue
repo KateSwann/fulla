eb1fef25ed8f463107d4e148b641749e4739008c
Екатерина Лебедева <>
2023-01-18 21:02:11 +0000

    - Сверстан список результатов выдачи

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { useScreen, useGrid } from 'vue-screen'
 
+import ResultItemCard from '@/components/ResultItemCard.vue'
+
 const screen = useScreen({})
 const grid = useGrid({
     sm: 0,
@@ -13,23 +15,84 @@ const grid = useGrid({
 </script>
 
 <template>
-  <main>
-    <div>
-        <p>Screen width is {{ screen.width }}</p>
-        <p>Screen height is {{ screen.height }}</p>
-        <p>Current breakpoint is {{ grid.breakpoint }}</p>
-    </div>
+    <main class="search-page">
+        <div>
+            <p>Screen width is {{ screen.width }}</p>
+            <p>Screen height is {{ screen.height }}</p>
+            <p>Current breakpoint is {{ grid.breakpoint }}</p>
+        </div>
 
-    <div v-show="grid.isMobile">
-        --------isMobile--------
-    </div>
+        <!-- START Содержание контента - mobile -->
+        <div class="search-page__content"
+             v-show="grid.isMobile">
+            <ResultItemCard
+                v-for="index in 10"
+                :key="index">
+            </ResultItemCard>
+        </div>
+        <!-- END Содержание контента - mobile -->
 
-    <div v-show="grid.isTablet">
-        --------isTablet--------
-    </div>
+        <!-- START Содержание контента - tablet -->
+        <div class="search-page__content"
+             v-show="grid.isTablet">
+            <ResultItemCard
+                v-for="index in 10"
+                :key="index">
+            </ResultItemCard>
+        </div>
+        <!-- END Содержание контента - tablet -->
 
-    <div v-show="grid.isDesktop">
-        --------isDesktop--------
-    </div>
-  </main>
+        <!-- START Содержание контента - desktop -->
+        <div class="search-page__content"
+             v-show="grid.isDesktop">
+            <div class="search-page__left-column">
+                <ResultItemCard
+                    v-for="index in 10"
+                    :key="index">
+                </ResultItemCard>
+            </div>
+
+            <div class="search-page__right-column">
+                RR
+            </div>
+        </div>
+        <!-- END Содержание контента - desktop -->
+    </main>
 </template>
+
+<style lang="scss">
+.search-page {
+    &__content {}
+
+    @media (max-width: 767px) {
+        &__content {}
+    }
+
+    @media (min-width: 768px) and (max-width: 1439px) {
+        &__content {
+            max-width: 728px;
+            padding: 20px;
+            margin: 0 auto;
+        }
+    }
+
+    @media (min-width: 1440px) {
+        &__content {
+            max-width: 1320px;
+            margin: 0 auto;
+            display: flex;
+        }
+
+        &__left-column {
+            flex: 1;
+            max-width: calc(50% - 60px);
+            background-color: gold;
+        }
+
+        &__right-column {
+            flex: 1;
+            background-color: antiquewhite;
+        }
+    }
+}
+</style>
